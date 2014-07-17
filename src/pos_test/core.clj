981233eea19538fe26304fdb77f4ce22d73af718
Grigoriy Glushko <grinderrz@gmail.com>
2014-07-17 14:06:50 +0400

    refactor store

--- src/pos_test/core.clj
+++ src/pos_test/core.clj
@@ -7,26 +7,29 @@
   (:import [com.mongodb MongoOptions ServerAddress])
   (:gen-class))
 
-(def conn (monger.core/connect))
-(def db (monger.core/get-db conn "patterns"))
-
-(defn store [v]
-  (let [sentence (first v)
-        model (nth v 1)]
-    (if (> (monger.collection/count db "model" {:model model}) 0)
-      (monger.collection/update db
-                                "model"
-                                {:model model}
-                                {monger.operators/$push {:sentences sentence}
-                                 monger.operators/$inc {:sc 1}}) 
-      (monger.collection/insert db
-                                "model"
-                                {:model model :sentences [sentence] :sc 1}))))
+(def store
+  (let [db (monger.core/get-db (monger.core/connect) "patterns")]
+    (fn [[sentence model]]
+      (if (> (monger.collection/count db "model" {:model model}) 0)
+        (monger.collection/update db
+                                  "model"
+                                  {:model model}
+                                  {monger.operators/$push {:sentences sentence}
+                                   monger.operators/$inc {:sc 1}}) 
+        (monger.collection/insert db
+                                  "model"
+                                  {:model model :sentences [sentence] :sc 1})))))
 
 (defn -main
   [& args]
-  (let [filename (first args)
-        text (slurp filename)
+  (from-file (first args))
+  )
+
+(defn from-file [filename]
+  (let [text (slurp filename)
         tagged (model/tag-text text)]
     (doall (map store tagged))))
 
+(defn from-mysql []
+  
+  )
